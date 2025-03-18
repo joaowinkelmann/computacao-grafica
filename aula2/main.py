2adc11049e5190dba9d38f8c610c102b56f8fa69
João Winkelmann <hmaster.studio@gmail.com>
2025-03-17 22:49:03 -0300

    chore: Adicionando requisitos, ajustando cor e código duplicado

--- aula2/main.py
+++ aula2/main.py
@@ -1,23 +1,22 @@
-import glfw;
-# from OpenGL.GL import *;
+# Requisitos
+# pip install glfw PyOpenGL
+
+import glfw
+from OpenGL.GL import *
 
 def main():
     if not glfw.init():
-        return;
-    janela = glfw.create_window(800, 600, "Hello World", None, None);
-    glfw.make_context_current(janela);
+        return
+    janela = glfw.create_window(800, 600, "Hello World", None, None)
+    glfw.make_context_current(janela)
 
     while not glfw.window_should_close(janela):
         if glfw.get_key(janela, glfw.KEY_ESCAPE) == glfw.PRESS:
-            glfw.set_window_should_close(janela, True);
-        glClearColor(0.2, 0.3, 0.2, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
-        glfw.swap_buffers(janela);
-        glfw.poll_events();
-        glfw.swap_buffers(janela);
-        print("teste");
-
+            glfw.set_window_should_close(janela, True)
+        glClearColor(0, 0.7, 0.4, 1) # r, g, b, transparencia
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glfw.swap_buffers(janela)
+        glfw.poll_events()
 
 if __name__ == "__main__":
     main()
